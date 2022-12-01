70007f6a3b224aaf687ad83607dde62d2cccb350
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-12-02 04:16:04 +0900

    add: add swiftpm files

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "XCContributeRank",
+    platforms: [.macOS(.v10_13)],
+    products: [
+        .executable(name: "xccrank", targets: ["XCContributeRank"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .executableTarget(
+            name: "XCContributeRank",
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]),
+        .target(name: "XCContributeRankCore"),
+        .testTarget(
+            name: "XCContributeRankTests",
+            dependencies: ["XCContributeRank"]),
+    ]
+)
---
+++ Sources/XCContributeRank/XCContributeRank.swift
@@ -0,0 +1,8 @@
+@main
+public struct XCContributeRank {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(XCContributeRank().text)
+    }
+}
