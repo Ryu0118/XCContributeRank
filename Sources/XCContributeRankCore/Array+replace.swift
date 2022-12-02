60fa1b96d57dd58ccdb873f5ca67603596dd16e7
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-12-02 14:33:40 +0900

    add: add Array extension

---
+++ Sources/XCContributeRankCore/Array+replace.swift
@@ -0,0 +1,15 @@
+//
+//  Array+replace.swift
+//  
+//
+//  Created by Ryu on 2022/12/02.
+//
+
+import Foundation
+
+extension Array {
+    mutating func replace(_ value: Element, at index: Int) {
+        remove(at: index)
+        insert(value, at: index)
+    }
+}
