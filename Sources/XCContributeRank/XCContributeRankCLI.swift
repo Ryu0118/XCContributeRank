c3ac83234d7b305aebc82b920f8d582688a440fb
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-12-04 16:43:25 +0900

    add: add execution section

--- Sources/XCContributeRank/XCContributeRankCLI.swift
+++ Sources/XCContributeRank/XCContributeRankCLI.swift
@@ -8,5 +8,17 @@ struct XCContributeRankCLI: ParsableCommand {
     var projectPath: String
     
     static let _commandName: String = "rank"
-}
+    
+    mutating func run() throws {
+        guard let url = URL(string: projectPath) else {
+            throw XCContributeRankError.invalidPath
+        }
+        
+        let xcContributeRank = XCContributeRank(root: url)
+        let contributions = try xcContributeRank.getContributions()
+        let decorator = Decorator(contributions: contributions)
+        let result = decorator.getDecoratedString()
 
+        print(result)
+    }
+}
