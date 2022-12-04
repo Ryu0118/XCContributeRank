ef9ff15c46744d36ca64c20ba79f431b3f288762
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-12-04 16:42:07 +0900

    add: add Extensions of String

---
+++ Sources/XCContributeRank/StringExtensions.swift
@@ -0,0 +1,39 @@
+//
+//  StringExtensions.swift
+//  
+//
+//  Created by Ryu on 2022/12/02.
+//
+
+import Foundation
+
+extension String {
+    mutating func newLine(_ string: String) {
+        self.append(string + "\n")
+    }
+    
+    mutating func whitespaces(_ count: Int) {
+        self.append(String(repeating: " ", count: count))
+    }
+    
+    func withDivider(divider: String) -> String {
+        var string = ""
+        string.newLine(divider)
+        string.newLine(self)
+        string.append(divider)
+        return string
+    }
+    
+    var width: Int {
+        var sum = 0
+        for char in Array(self).map({String($0)}) {
+            let doubleByteCharacterSet = CharacterSet(charactersIn: "\u{4E00}"..."\u{9FFF}")
+            let isDoubleByteString = char.allSatisfy { character in
+                return doubleByteCharacterSet.contains(character.unicodeScalars.first!)
+            }
+            sum += isDoubleByteString ? 2 : 1
+        }
+        return sum
+    }
+    
+}
